6e19806b2ef00e41a5826113305132ccb8c0d84f
Your Name <you@example.com>
2024-10-18 15:30:45 +0200

    testing sonarcloud 3

--- fide_download.py
+++ fide_download.py
@@ -26,7 +26,7 @@ HASH_FILE_NAME = "last_hash.txt"
 
 # Function to compute the hash of a file
 def compute_file_hash(file_name):
-    kk = 0
+    kk = 10
     hash_sha256 = hashlib.sha256()
     try:
         with open(file_name, "rb") as f:
